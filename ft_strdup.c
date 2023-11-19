7035f27e3d39ae62d0c57fea6ed5212cf1048578
Yoseph <yosephman76@gmail.com>
2023-11-19 06:22:46 +0400

    commit

---
+++ ft_strdup.c
@@ -0,0 +1,29 @@
+#include "libft.h"
+
+// #include <stdio.h>
+// #include "ft_strlen.c"
+// #include "string.h"
+
+char	*ft_strdup(const char *s1)
+{
+	char	*ptr;
+	size_t	i;
+
+	i = 0;
+	ptr = (char *)malloc(sizeof(char) * (ft_strlen(s1) + 1));
+	if (!ptr)
+		return (ptr);
+	while (s1[i])
+	{
+		ptr[i] = s1[i];
+		i++;
+	}
+	ptr[i] = '\0';
+	return (ptr);
+}
+
+// int main(int ac, char *av[])
+// {
+//     printf("%s\n",ft_strdup(av[1]));
+//     printf("%s",strdup(av[1]));
+// }

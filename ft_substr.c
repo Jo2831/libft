89436b31ec5c920937b55f186b7ab7e390cd6524
Yoseph <yosephman76@gmail.com>
2023-11-19 06:24:26 +0400

    commit

---
+++ ft_substr.c
@@ -0,0 +1,36 @@
+#include "libft.h"
+
+// #include <stdio.h>
+// #include "ft_strlen.c"
+// #include <string.h>
+
+char	*ft_substr(char const *s, unsigned int start, size_t len)
+{
+	char			*new;
+	unsigned int	i;
+
+	if (start >= ft_strlen(s))
+		len = 0;
+	else if (len > ft_strlen(s + start))
+		len = ft_strlen(s + start);
+	new = malloc(sizeof(char) * (len + 1));
+	if (!new)
+		return (NULL);
+	i = 0;
+	while (start + i < ft_strlen(s) && i < len)
+	{
+		new[i] = s[start + i];
+		i++;
+	}
+	new[i] = '\0';
+	return (new);
+}
+
+// int main(int ac, char *av[])
+// {
+//     char *st = (av[2]);
+//     size_t start = atoi(st);
+//     size_t len = sizeof(av[1]);
+//     printf("%s\n", ft_substr(av[1], start, len));
+
+// }

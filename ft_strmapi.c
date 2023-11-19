2d2e5c978282e377cc99a624e3b36867b8ab795b
Yoseph <yosephman76@gmail.com>
2023-11-19 06:23:39 +0400

    commit

---
+++ ft_strmapi.c
@@ -0,0 +1,51 @@
+#include "libft.h"
+
+char	*ft_strmapi(char const *s, char (*f)(unsigned int, char))
+{
+	unsigned int	i;
+	unsigned int	length;
+	char			*res;
+
+	if (!s)
+		return (NULL);
+	length = ft_strlen(s);
+	res = malloc(length * sizeof(char) + 1);
+	if (!res)
+		return (NULL);
+	i = 0;
+	while (i < length)
+	{
+		res[i] = (*f)(i, s[i]);
+		i++;
+	}
+	res[i] = '\0';
+	return (res);
+}
+
+// char map_function(unsigned int index, char c)
+// {
+//     if (c >= 'a' && c <= 'z')
+//         return (c - 'a' + 'A');
+//     return (c);
+// }
+
+// int main()
+// {
+//     const char *input_string = "Hello, World!";
+
+//     char *result = ft_strmapi(input_string, &map_function);
+
+//     if (result)
+//     {
+//         printf("Original string: %s\n", input_string);
+//         printf("Mapped string:   %s\n", result);
+
+//         free(result);
+//     }
+//     else
+//     {
+//         printf("Memory allocation failed.\n");
+//     }
+
+//     return (0);
+// }
